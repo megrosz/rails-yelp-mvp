366f713d6241e30475f2156075fcfdfc8c458c46
Megan Grosz <megrosz@gmail.com>
2021-01-14 21:29:03 +0100

    added routes and views

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,20 @@
 class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    restaurant = Restaurant.new(restaurant_params)
+    restaurant.save
+    redirect_to restaurants_path
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "cleaning up database"
+Restaurant.destroy_all
+puts 'database is clean'
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: Restaurant::CATEGORY.sample
+  )
+  restaurant.save
+end
+
+puts 'finished!'
